e291a90fef3eca9e0443215c397d041c3735a515
SDJoky <1270849866@qq.com>
2019-08-01 13:03:00 +0800

    add .

---
+++ SwiftProject/Controller/ThemeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ThemeViewController.swift
+//  SwiftProject
+//
+//  Created by Joky_Lee on 2019/8/1.
+//  Copyright © 2019 Joky_Lee. All rights reserved.
+//
+
+import UIKit
+
+class ThemeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+    
+
+}
--- SwiftProject/Controller/ThirdViewController.swift
+++ SwiftProject/Controller/ThirdViewController.swift
@@ -118,7 +118,7 @@ extension ThirdViewController {
 extension ThirdViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let detailVC = Test1DetailViewController()
+        let detailVC = ThemeViewController()
         self.navigationController?.pushViewController(detailVC, animated: true)
         
     }
